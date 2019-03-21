b1c3ab649b6051833a58d8f0468f426134bfe2b9
John Iyere <john.iyere@student.manchester.ac.uk>
2019-03-21 12:13:04 +0000

    Added comments to adapted code

--- tf_print.py
+++ tf_print.py
@@ -1,3 +1,7 @@
+# Adapted from this code: 
+# https://gist.github.com/vihari/f9b361058825e16d390f0e443bfdffc7
+# https://stackoverflow.com/questions/49012912/error-when-calling-global-variables-initializer-in-tensorflow 
+# https://danijar.com/what-is-a-tensorflow-session/
 import tensorflow as tf
 
 def tf_print(val):
